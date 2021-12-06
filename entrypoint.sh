dac67e58d813bc80838e0c1fe823d734c77426aa
Marco Ortiz <mortiz@seniormarketsales.com>
2021-12-06 11:22:32 -0500

    skip wwi

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 #download wwi db
-wget -P /var/opt/mssql/data/ https://github.com/Microsoft/sql-server-samples/releases/download/wide-world-importers-v1.0/WideWorldImporters-Full.bak
-chmod +x /var/opt/mssql/data/WideWorldImporters-Full.bak
+#wget -P /var/opt/mssql/data/ https://github.com/Microsoft/sql-server-samples/releases/download/wide-world-importers-v1.0/WideWorldImporters-Full.bak
+#chmod +x /var/opt/mssql/data/WideWorldImporters-Full.bak
 
 # Run init-script with long timeout - and make it run in the background with "&"
 # restore wwi db
-/opt/mssql-tools/bin/sqlcmd -S localhost -l 60 -U SA -P ${MSSQL_SA_PASSWORD} -i /opt/mssql-scripts/restore.sql &
+#/opt/mssql-tools/bin/sqlcmd -S localhost -l 60 -U SA -P ${MSSQL_SA_PASSWORD} -i /opt/mssql-scripts/restore.sql &
 
 # the /var/opt/mssql/data directory was created by the mssql image
 # create empty restore files as mssql user
-touch /var/opt/mssql/data/WideWorldImporters.mdf
-touch /var/opt/mssql/data/WideWorldImporters_UserData.ndf
-touch /var/opt/mssql/data/WideWorldImporters.ldf
+#touch /var/opt/mssql/data/WideWorldImporters.mdf
+#touch /var/opt/mssql/data/WideWorldImporters_UserData.ndf
+#touch /var/opt/mssql/data/WideWorldImporters.ldf
 
 # restore demodb
 /opt/mssql-tools/bin/sqlcmd -S localhost -l 60 -U SA -P ${MSSQL_SA_PASSWORD} -i /opt/mssql-scripts/demodb.sql &
